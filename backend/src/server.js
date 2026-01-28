f74d9e610ea18f45f8cf24e710bb45b31d77eaf8
longnguyenha050 <22520965@gm.uit.edu.vn>
2026-01-28 20:55:49 +0700

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,47 +1,49 @@
 import "./loadEnv.js";
 
-import express from 'express';
+import express from "express";
 import cors from "cors";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 
-import { connectDB } from './libs/db.js';
+import { connectDB } from "./libs/db.js";
 import "./config/cloudinary.js";
-import authRoute from './routes/auth.route.js'
+import authRoute from "./routes/auth.route.js";
 import adminRoute from "./routes/admin.route.js";
-import adminOrderRoute from "./routes/adminOrder.route.js"
+import adminOrderRoute from "./routes/adminOrder.route.js";
 import userRoute from "./routes/user.route.js";
-import productRoute from "./routes/product.route.js"
-import promotionRoute from "./routes/promotion.route.js"
-import favouriteRoute from "./routes/favourite.route.js"
-import addressRoute from "./routes/address.route.js"
-import reviewRoute from "./routes/review.route.js"
+import productRoute from "./routes/product.route.js";
+import promotionRoute from "./routes/promotion.route.js";
+import favouriteRoute from "./routes/favourite.route.js";
+import addressRoute from "./routes/address.route.js";
+import reviewRoute from "./routes/review.route.js";
 // import cartRoute from "./routes/cart.route.js"
-import cartRoute from "./routes/cart.route.js"
+import cartRoute from "./routes/cart.route.js";
 // import orderRoute from "./routes/order.route.js"
-import postRoute from "./routes/post.route.js"
+import postRoute from "./routes/post.route.js";
 
-import payosRoute from "./routes/payos.route.js"
+import payosRoute from "./routes/payos.route.js";
 
-import blogRoute from "./routes/blog.route.js"
-import contactRoute from "./routes/contact.route.js"
-import dashboardRoute from "./routes/dashboard.route.js"
-
+import blogRoute from "./routes/blog.route.js";
+import contactRoute from "./routes/contact.route.js";
+import dashboardRoute from "./routes/dashboard.route.js";
 
 import { protectedRoute } from "./middlewares/authMiddleware.js";
-import { adminOnly } from './middlewares/adminMiddleware.js';
+import { adminOnly } from "./middlewares/adminMiddleware.js";
 // import devRoute from "./routes/devRoute.js";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:5175",
+      "https://shate-shoes-store-kk6j.vercel.app",
+    ],
     credentials: true,
-  })
+  }),
 );
-
 
 app.use(cookieParser());
 app.use(express.json());
@@ -52,18 +54,39 @@ app.use("/api/blog", blogRoute);
 app.use("/api/guest", productRoute);
 
 // private user routes (CẦN đăng nhập)
-app.use("/api/users", protectedRoute, userRoute, productRoute, favouriteRoute, addressRoute, cartRoute, promotionRoute, adminOrderRoute, payosRoute, reviewRoute, postRoute, contactRoute);
+app.use(
+  "/api/users",
+  protectedRoute,
+  userRoute,
+  productRoute,
+  favouriteRoute,
+  addressRoute,
+  cartRoute,
+  promotionRoute,
+  adminOrderRoute,
+  payosRoute,
+  reviewRoute,
+  postRoute,
+  contactRoute,
+);
 
 // admin routes (CẦN đăng nhập + quyền admin)
-app.use("/api/admin", protectedRoute, adminOnly, adminRoute, productRoute, promotionRoute, userRoute, postRoute, adminOrderRoute, reviewRoute, dashboardRoute);
-
-
-
+app.use(
+  "/api/admin",
+  protectedRoute,
+  adminOnly,
+  adminRoute,
+  productRoute,
+  promotionRoute,
+  userRoute,
+  postRoute,
+  adminOrderRoute,
+  reviewRoute,
+  dashboardRoute,
+);
 
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server bắt đầu trên cổng ${PORT}`);
   });
 });
-
-
